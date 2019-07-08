e25bebc3e88cfa99bf0cd9e46d4ecf14a9884d4b
baloran <contact@baloran.fr>
2019-07-08 12:08:34 +0200

    feat(proxy): add handle for promise response

--- proxy.js
+++ proxy.js
@@ -1,27 +1,52 @@
-const serializeError = require('serialize-error');
-const path = require('path');
+const serializeError = require('serialize-error')
+const path = require('path')
 
-function handler(event, context, callback) {
+const use_promise = process.env.SERVERLESS_OFFLINE_PROMISE || false
 
-  const [targetHandlerFile, targetHandlerFunction] = event.targetHandler.split(".");
-  const target = require(path.resolve(__dirname, '../..', targetHandlerFile));
+let handler = null
 
-  target[targetHandlerFunction](event.body, context, (error, response) => {
+if (use_promise) {
+  handler = async (event, context) => {
+    try {
+      const [
+        targetHandlerFile,
+        targetHandlerFunction,
+      ] = event.targetHandler.split('.')
+      const target = require(path.resolve(
+        __dirname,
+        '../..',
+        targetHandlerFile,
+      ))
 
-    if (error) {
+      const response = await target[targetHandlerFunction](event.body, context)
+
+      return Promise.resolve(response)
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
+} else {
+  handler = (event, context, callback) => {
+    const [
+      targetHandlerFile,
+      targetHandlerFunction,
+    ] = event.targetHandler.split('.')
+    const target = require(path.resolve(__dirname, '../..', targetHandlerFile))
+
+    target[targetHandlerFunction](event.body, context, (error, response) => {
+      if (error) {
         // Return Serverless error to AWS sdk
         callback(null, {
-            StatusCode: 500,
-            FunctionError: 'Handled',
-            Payload: serializeError(error)
+          StatusCode: 500,
+          FunctionError: 'Handled',
+          Payload: serializeError(error),
         })
-    } else {
+      } else {
         // Return lambda function response to AWS SDK & pass through args from serverless.
         callback(null, response)
-    }
-  });
-
-
+      }
+    })
+  }
 }
 
-module.exports.handler = handler;
+module.exports.handler = handler
